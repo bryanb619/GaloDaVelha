e150cf5538ef134551868c25d9dfd9d728bd7907
Steven Hall <stevenbryanb.work@gmail.com>
2024-04-01 20:19:28 +0200

    Updated XML Program.cs

--- ProjectGaloDaVelha/Program.cs
+++ ProjectGaloDaVelha/Program.cs
@@ -3,21 +3,25 @@
 namespace ProjectGaloDaVelha
 {
     /// <summary>
-    /// 
+    ///  Simple Program class that creates instance of game class
+    ///  and calls the start method, starting the GaloDaVelha game
     /// </summary>
     class Program
     {
         
         /// <summary>
-        ///  
-        /// </summary>
+        ///  Main initializes Program
+        ///  Declares a instance of Game class
+        ///  Calls the Start method of the Game class
+        ///  Starts game (GaloDaVelha) Game class
+        /// /// </summary>
         /// <param name="args">Not used</param>
         private static void Main(string[] args)
         {
             // create game instance
             Game game = new Game();
             
-            // start game instance
+            // start game method
             game.Start();
 
             
