e798f2067d8feb98822a3fd093e918e173d3a8db
Steven Hall <stevenbryanb.work@gmail.com>
2024-04-05 23:53:07 +0100

    working on Piece Size XML

--- ProjectGaloDaVelha/Pieces/PieceSize.cs
+++ ProjectGaloDaVelha/Pieces/PieceSize.cs
@@ -1,8 +1,18 @@
 namespace ProjectGaloDaVelha.Pieces
 {
+    /// <summary>
+    /// Enumerator defines the size of the piece
+    /// </summary>
     public enum PieceSize
     {
+        /// <summary>
+        /// 
+        /// </summary>
         big,
+
+        /// <summary>
+        /// 
+        /// </summary>
         small
     }
 }
