27c004b50db28e7e52e6efe9ece56b0ca4eed3bd
Steven Hall <stevenbryanb.work@gmail.com>
2024-04-05 19:58:42 +0100

    Added comments to PieceHole.cs

--- ProjectGaloDaVelha/Pieces/PieceHole.cs
+++ ProjectGaloDaVelha/Pieces/PieceHole.cs
@@ -1,5 +1,7 @@
 namespace ProjectGaloDaVelha.Pieces
 {
+    // public enumerator of types of holes a piece can have
+    // They can be either none or hole 
     public enum PieceHole
     {
         none,
