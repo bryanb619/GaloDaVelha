1ae2ef05e0ca15f576a6929ec8080ade7cf9d452
Steven Hall <stevenbryanb.work@gmail.com>
2024-04-05 23:52:31 +0100

    Finished: XML Piece Shape

--- ProjectGaloDaVelha/Pieces/PieceShape.cs
+++ ProjectGaloDaVelha/Pieces/PieceShape.cs
@@ -1,9 +1,18 @@
 namespace ProjectGaloDaVelha.Pieces
 {
-
+    /// <summary>
+    ///  Enumerator defines if piece is a circle or a square
+    /// </summary>
     public enum PieceShape
     {
+        /// <summary>
+        /// Piece of cicle shape
+        /// </summary>
         circle,
+        
+        /// <summary>
+        /// Piece of square shape
+        /// </summary>
         square
     }
 
