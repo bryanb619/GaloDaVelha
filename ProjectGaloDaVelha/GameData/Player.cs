bbf790ecb4d25d3b5d3c4f86a83763ec0fc1290e
Steven Hall <stevenbryanb.work@gmail.com>
2024-04-06 01:19:32 +0100

    Finished Player Enum XML

--- ProjectGaloDaVelha/GameData/Player.cs
+++ ProjectGaloDaVelha/GameData/Player.cs
@@ -1,8 +1,20 @@
 namespace ProjectGaloDaVelha.GameData
 {
+    /// <summary>
+    /// Defines player turns & which player is playing is the current turn
+    /// </summary>
     public enum Player
     {
+        /// <summary>
+        /// Represents player 1
+        /// Acts as global reference to player 1 
+        /// </summary>
         Player1,
+
+        /// <summary>
+        /// Represents player 2
+        /// Acts as global reference to player 2 
+        /// </summary>
         Player2
     }
 
