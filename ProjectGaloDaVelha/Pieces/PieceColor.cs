e9b4bcc31055a1be62e84847c30f4d9643903cd1
Steven Hall <stevenbryanb.work@gmail.com>
2024-04-06 01:14:12 +0100

    Finished PieceColor Enum XML

--- ProjectGaloDaVelha/Pieces/PieceColor.cs
+++ ProjectGaloDaVelha/Pieces/PieceColor.cs
@@ -1,8 +1,18 @@
 namespace ProjectGaloDaVelha.Pieces
 {
+    /// <summary>
+    /// Defines color of the Piece
+    /// </summary>
     public enum PieceColor
     {
+        /// <summary>
+        /// reference to the green color
+        /// </summary>
         green,
+
+        /// <summary>
+        /// reference to the yellow color
+        /// </summary>
         yellow
     }
 }
