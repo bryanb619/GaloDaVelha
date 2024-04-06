57db1edd3253556c718972dae1eb0f4ce06e4b64
Steven Hall <stevenbryanb.work@gmail.com>
2024-04-06 01:17:28 +0100

    XML: Working on Board.cs Added to Getter & class

--- ProjectGaloDaVelha/GameData/BoardMapping/Board.cs
+++ ProjectGaloDaVelha/GameData/BoardMapping/Board.cs
@@ -2,11 +2,12 @@ namespace ProjectGaloDaVelha.GameData.BoardMapping
 {
 
     /// <summary>
-    /// Class that represents the game board
-    ///  
+    /// Class that represents the game board layout
+    /// stores and protects board layout from outside access
     /// /// </summary>
     public class Board 
-    {
+    {   
+        // board layout
         private string[,] boardMap =
         {
             {"A","B","C","D"},
@@ -16,9 +17,10 @@ namespace ProjectGaloDaVelha.GameData.BoardMapping
         };
 
         /// <summary>
-        ///  
+        /// Getter for the board map
+        /// this is a visual representation of the board
         /// </summary>
-        /// <returns>returns board layout</returns>
+        /// <returns>returns board layout as bidimensional array</returns>
         public string[,] GetBoard()
         {
             // board layout
